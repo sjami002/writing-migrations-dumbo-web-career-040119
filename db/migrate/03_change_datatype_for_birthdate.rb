f8b9e70c21a935f5ceed9a8dbcc8cf1619ba2568
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-12 13:13:00 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,1 +1,6 @@
-class ChangeDatatypeForBirthdate < ActiveRecord :: Migration
+class ChangeDatatypeForBirthdate < ActiveRecord :: Migration[5.1]
+  
+  def change
+    change_column :students, 
+  end
+end
